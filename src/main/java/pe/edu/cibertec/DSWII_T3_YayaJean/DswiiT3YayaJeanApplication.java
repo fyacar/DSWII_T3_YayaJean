c253401c9e1f5a6193cabbc6c736cd8addb2a50d
Jean Y <fyacar_14@hotmail.com>
2024-07-02 13:39:49 -0500

    Primera Pregunta - Primer commit

---
+++ src/main/java/pe/edu/cibertec/DSWII_T3_YayaJean/DswiiT3YayaJeanApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_T3_YayaJean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiT3YayaJeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiT3YayaJeanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DSWII_T3_YayaJean/DswiiT3YayaJeanApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_T3_YayaJean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiT3YayaJeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
