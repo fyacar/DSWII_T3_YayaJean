bbaac2260962657557f8716d285d0d45e451f4f9
Jean Y <fyacar_14@hotmail.com>
2024-07-02 23:08:21 -0500

    Tercera pregunta - primer commit

---
+++ src/main/java/pe/edu/cibertec/DSWII_T3_YayaJean/controller/Pregunta3Controller.java
@@ -0,0 +1,12 @@
+package pe.edu.cibertec.DSWII_T3_YayaJean.controller;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping(path = "api/v1/filesdoc")
+public class Pregunta3Controller {
+}
---
+++ src/main/java/pe/edu/cibertec/DSWII_T3_YayaJean/service/IFileService.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_T3_YayaJean.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface IFileService {
+
+    void guardarArchivo(MultipartFile archivo) throws Exception;
+
+    void guardarArchivos(List<MultipartFile> archivosList) throws Exception;
+
+}
